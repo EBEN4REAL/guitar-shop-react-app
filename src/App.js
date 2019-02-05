58864ee3f2aaf4be54e68d957785802bcb21a2c9
Igbinoba Ebenezer <ebenezer.igbinoba@stu.cu.edu.ng>
2019-02-05 16:49:04 +0100

    Added all dependencies

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+
+  componentDidMount(){
+    axios.get("http://localhost:3002/api/product/brands").then(response => {
+      console.log(response);
+    })
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          MY APP
         </header>
       </div>
     );
