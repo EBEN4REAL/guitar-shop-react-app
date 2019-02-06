a78ad734d9c0266921966e7540d21303dc11bf38
Igbinoba Ebenezer <ebenezer.igbinoba@stu.cu.edu.ng>
2019-02-06 19:17:19 +0100

    Addd the Login and register components

--- src/components/utils/Button.js
+++ src/components/utils/Button.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import Link from 'react-router-dom'
 
 
-const Button = (props) => {
+const button = (props) => {
+
+    const buttons = () => {
+        let template = '';
+        switch(props.type){
+            case "default": 
+                template = <Link></Link>
+                break;
+            default:
+                template = '';
+        }
+    }
     return (
-        <div>
-            <button>{props.title}</button>
+        <div className="my_link">
+            {button()}
         </div>
     )
 }
