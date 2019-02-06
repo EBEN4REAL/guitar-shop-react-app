69c33da96ba2fc308a0737cb9d92406f8829433c
Igbinoba Ebenezer <ebenezer.igbinoba@stu.cu.edu.ng>
2019-02-06 15:20:38 +0100

    Added the Footer componentas

--- src/components/Header_footer/Footer/index.js
+++ src/components/Header_footer/Footer/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import faCompass from '@fortawesome/fontawesome-free-solid/faCompass' // fonts Awesome location
 
 const Footer = (props) => {
     return (
