d0cda675f96cd73901ce6c2e2507b6f220c37860
Igbinoba Ebenezer <ebenezer.igbinoba@stu.cu.edu.ng>
2019-02-06 16:44:39 +0100

    created the store , actions and reducers files

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './Resources/css/styles.css';
 
+
 import {BrowserRouter} from 'react-router-dom';
 import Routes from './routes';
+
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
 
 ReactDOM.render(
     <BrowserRouter>
