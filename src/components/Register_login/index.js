9031fe21f0bd517b026edbb42a510c9d972a1d0c
Igbinoba Ebenezer <ebenezer.igbinoba@stu.cu.edu.ng>
2019-02-06 18:49:18 +0100

    Adde Redux and Reducers

--- src/components/Register_login/index.js
+++ src/components/Register_login/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 const RegisterLogin = (props) => {
     return (
-        <div>
-            
+        <div className="page_wrapper">
+            <div className="container">
+                <div className="register_login_container">
+                    <div className="left">
+                        <h1>New Customers</h1>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,15 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Home from './components/Home';
 import Layout from './hoc/Layout';
+import RegisterLogin from  './components/Register_login';
+
 
 const Routes = () => {
   return (
     <Layout>
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/register_login" exact component={RegisterLogin} />
       </Switch>
     </Layout>
     
