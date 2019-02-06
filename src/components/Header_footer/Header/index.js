c4b5a6d43580721d5815d4fe71863542c83c3492
Igbinoba Ebenezer <ebenezer.igbinoba@stu.cu.edu.ng>
2019-02-06 14:32:54 +0100

    Added the Header and Footer component

--- src/components/Header_footer/Header/index.js
+++ src/components/Header_footer/Header/index.js
@@ -4,8 +4,20 @@ class Header extends Component {
     render(){
         return (
             <header className="bck_b_light">
-                <div>
-                
+                <div className="container">
+                    <div className="left">
+                        <div className="logo">
+                            Waves
+                        </div>
+                    </div>
+                     <div className="right">
+                        <div className="top">
+                            LINKS
+                        </div>
+                         <div className="bottom">
+                            LINKS
+                        </div>
+                    </div>
                 </div>
             </header>
         )
--- src/hoc/Layout.js
+++ src/hoc/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Component} from 'react';
+import Header from '../components/Header_footer/Header';
 
 class Layout extends Component {
     render(){
         return (
             <div>
-                Header
+                <Header />
                  <div className="page_container">
                     {this.props.children}
                 </div>
