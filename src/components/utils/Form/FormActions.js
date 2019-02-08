1e599526aef6ae7fd1df4281ab5258aea9d9a607
Igbinoba Ebenezer <ebenezer.igbinoba@stu.cu.edu.ng>
2019-02-08 12:17:25 +0100

    Added the Form actions script

--- src/components/utils/Form/FormActions.js
+++ src/components/utils/Form/FormActions.js
@@ -1,13 +1,21 @@
 
 
+export const  validate = () => {
 
- const update = (element, formData, formName) => {
+}
+
+
+ export const update = (element, formData, formName) => {
     const newFormData = {
         ...formData,
     }
     const newElement  = {
         ...newFormData[element.id]
     }
+    newElement.value = element.event.target.value;
+    
+    if(element.blur){
+        let validData = validate(newElement);
+    }
 }
 
-export default update;
