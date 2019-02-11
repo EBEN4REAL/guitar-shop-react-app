0a0b49206591563df6830e070959614230d250c9
Igbinoba Ebenezer <ebenezer.igbinoba@stu.cu.edu.ng>
2019-02-11 18:15:15 +0100

    preventing user routes

--- src/hoc/auth.js
+++ src/hoc/auth.js
@@ -3,17 +3,32 @@ import {connect} from 'react-redux';
 import {auth} from '../store/actions/user_actions/user_actions';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
-export  const Auth = (component, boolean, adminRoute = null) => {
+export const Auth = (component, boolean, adminRoute = null) => {
     class AuthenticationCheck extends Component {
+        state = {
+            loading: true
+        }
         render(){
+            if(this.state.loading){
+                return (
+                    <div className="main_loader">
+                    
+                    </div>
+                )
+            }
             return (
-                <div >
+                <div>
     
                 </div>
             )
         }
     }
-    return connect()(AuthenticationCheck);
+    function  mapStateToProps(state){
+        return {
+            user: state.user
+        }
+    }
+    return connect(mapStateToProps)(AuthenticationCheck);
 }
 
 
