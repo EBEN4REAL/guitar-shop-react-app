54f885c88c8f96655d3b0f91cc0131d9f747ea4d
Igbinoba Ebenezer <ebenezer.igbinoba@stu.cu.edu.ng>
2019-02-11 17:36:58 +0100

    preventing user routes

--- src/hoc/auth.js
+++ src/hoc/auth.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {auth} from '../store/actions/user_actions/user_actions';
 
 class AuthenticationCheck extends Component {
     render(){
         return (
             <div>
-                
+
             </div>
         )
     }
