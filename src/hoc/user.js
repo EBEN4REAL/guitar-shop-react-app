4a5323343817d6813d9db83abea3f44a83719f34
Igbinoba Ebenezer <ebenezer.igbinoba@stu.cu.edu.ng>
2019-02-11 16:22:25 +0100

    Added User dashboard

---
+++ src/hoc/user.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const Links = [
+    {
+        name: "My acount",
+        linkTo: '/user/dashboard'
+    },
+    {
+        name: 'User Information',
+        linkTo: 'user/user_profile'
+    },
+    {
+        name: 'My Cart',
+        linkTo: 'user/user_profile'
+    }
+]
+
+const UserLayout = () => {
+    return (
+        <div>
+
+
+        </div>
+    )
+}
+
+export default UserLayout;
