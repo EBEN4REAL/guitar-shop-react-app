3a062bbfac47696c0598476a26717816ff0e9569
Igbinoba Ebenezer <ebenezer.igbinoba@stu.cu.edu.ng>
2019-02-11 17:16:04 +0100

    Added User dashboard

--- src/components/User/index.js
+++ src/components/User/index.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import UserLayout from '../../hoc/user';
+import Button from '../utils/Button';
 
 const  UserDashboard = () =>{
     return (
         <div>
-            <UserLayout />
+            <UserLayout>
+                <div className="user_nfo_panel">
+                    <h1>User information</h1>
+                    <div>
+                        <span>Name</span>
+                        <span>Last Name</span>
+                        <span>Email</span>
+                    </div>
+                    <Button
+                        type="default"
+                        title="Edit acount Info"
+                        linkTo="/user/user_profile"
+                        >
+                    
+                    </Button>
+                </div>
+                <div className="user_nfo_panel">
+                    <h1>Histroy purchases</h1>
+                    <div>
+                        
+                    </div>
+                </div>
+               
+            </UserLayout>
         </div>
     )
 }
