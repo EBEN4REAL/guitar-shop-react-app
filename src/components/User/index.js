1f8657fae1c7f7e9331159b88c120bd94c70b58c
Igbinoba Ebenezer <ebenezer.igbinoba@stu.cu.edu.ng>
2019-02-13 15:34:05 +0100

    Added the privacy to specific routes

--- src/components/User/index.js
+++ src/components/User/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import UserLayout from '../../hoc/user';
 import Button from '../utils/Button';
+import {connect} from 'react-redux';
 
-const  UserDashboard = () =>{
+const  UserDashboard = (props) =>{
+
+    console.log(props);
     return (
         <div>
             <UserLayout>
                 <div className="user_nfo_panel">
                     <h1>User information</h1>
                     <div>
-                        <span>Name</span>
+                        <span>Name: {props.user.user_data.name}</span>
                         <span>Last Name</span>
                         <span>Email</span>
                     </div>
@@ -33,4 +36,11 @@ const  UserDashboard = () =>{
     )
 }
 
+// const mapStateToProps = (state) => {
+//     return {
+//         userData: state.user_data
+//     }
+// }
+
+// export default connect(mapStateToProps)(UserDashboard);
 export default UserDashboard;
