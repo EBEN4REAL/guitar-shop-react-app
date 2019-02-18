d3d07627e6a5b67a1dfaa906d2dfc367310d96ac
Igbinoba Ebenezer <ebenezer.igbinoba@stu.cu.edu.ng>
2019-02-18 15:31:25 +0100

    created the card block component

--- src/components/utils/CardBlock.jsx
+++ src/components/utils/CardBlock.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
 const Cardblock = (props) => {
-    const renderCards = (cardList) => {
-        
-    }
+    const renderCards = (cardList) => (
+        props.list ? 
+            props.list.map((Cardblock) => {
+                return (
+                    <div>
+                        'CARD'
+                    </div>
+                ) 
+            })
+
+        : null
+    )
     return (
         <div className="card_block">
             <div className="container">
