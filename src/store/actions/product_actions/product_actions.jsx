9e7a0e51bfd799e5b7e0314360c53f37167198d6
Igbinoba Ebenezer <ebenezer.igbinoba@stu.cu.edu.ng>
2019-02-18 13:15:34 +0100

    created the product actions file

--- src/store/actions/product_actions/product_actions.jsx
+++ src/store/actions/product_actions/product_actions.jsx
@@ -5,3 +5,11 @@ import {AUTH_USER} from '../types';
 import  {LOGOUT_USER} from '../types.js';
 
 import {PRODUCT_SERVER}  from '../../../components/utils/misc';
+
+export function getProductsByArrival (){
+
+}
+
+export function getProductsBySale (){
+    
+}
