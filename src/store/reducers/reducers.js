6a2e41d2637dc0d32a4b79e6203757918030b8cb
Igbinoba Ebenezer <ebenezer.igbinoba@stu.cu.edu.ng>
2019-02-18 13:49:23 +0100

    created the product reducer file

--- src/store/reducers/reducers.js
+++ src/store/reducers/reducers.js
@@ -1,9 +1,11 @@
 
 import {combineReducers} from 'redux';
 import user from '../reducers/user_reducer/user_reducer';
+import products from '../reducers/products_reducer/product_reducer';
 
 const rootReducer = combineReducers({
-    user
+    user,
+    products
 })
 
 export default rootReducer;
