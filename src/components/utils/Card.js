41c531f13cba5bb62819af38b46d2c5165a790c3
Igbinoba Ebenezer <ebenezer.igbinoba@stu.cu.edu.ng>
2019-02-20 14:22:08 +0100

    created the card block component

--- src/components/utils/Card.js
+++ src/components/utils/Card.js
@@ -1,13 +1,20 @@
 import React, {Component}  from 'react';
 
 class Card extends Component {
+    renderCardImage(images){
+        if(images.length > 0){
+            return images[0].url
+        }else{
+
+        }
+    }
     render(){
         const props = this.props;
         return (
             <div className={`card_item_wrapper ${props.grid}`}>
                 <div className="image"
-                    STYLE={{ 
-                        BACKGROUND: `url(${this.renderCardImage})`
+                    style={{ 
+                        background: `url(${this.renderCardImage(props.images)}) no-repeat`
                      }}>
 
                 </div>
