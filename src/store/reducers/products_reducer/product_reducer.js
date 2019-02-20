89f8e9a2f56ab8e613faab7cd04044fd1ba48029
Igbinoba Ebenezer <ebenezer.igbinoba@stu.cu.edu.ng>
2019-02-20 17:11:16 +0100

    Created the product actions file

--- src/store/reducers/products_reducer/product_reducer.js
+++ src/store/reducers/products_reducer/product_reducer.js
@@ -1,18 +1,24 @@
 import {
     GET_PRODUCTS_BY_ARRIVAL,
-    GET_PRODUCTS_BY_SALE
+    GET_PRODUCTS_BY_SALE,
+    GET_BRANDS,
+    GET_WOODS
    } from '../../../store/actions/types';
 
 
 const  rootReducer =  (state={} , action) => {
     
    switch(action.type){
-       case GET_PRODUCTS_BY_ARRIVAL:
-            return  {...state, byArrival: action.payload};
-       case GET_PRODUCTS_BY_SALE:
-            return {...state, bySale: action.payload}
-       default:
-           return state;
+          case GET_PRODUCTS_BY_ARRIVAL:
+               return  {...state, byArrival: action.payload};
+          case GET_PRODUCTS_BY_SALE:
+               return {...state, bySale: action.payload}
+          case GET_BRANDS:
+               return {...state, get_brands: action.payload} 
+          case GET_WOODS:
+                return {...state, get_woods: action.payload} 
+          default:
+               return state;
    }
 }
 export default rootReducer;
